b195d39fc8bbeeb3c8fd7ba818fe0a78d2c5b3ee
Jesse <moore.jesse@gmail.com>
2020-12-21 10:23:25 -0600

    inialized studios

---
+++ mission-planning-dashboard/src/app/crew/crew.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-crew',
+  templateUrl: './crew.component.html',
+  styleUrls: ['./crew.component.css', '../app.component.css'],
+})
+export class CrewComponent implements OnInit {
+  crew: string[] = ['Jessica Watkins', 'Raja Chari', 'Jasmin Moghbeli'];
+  constructor() {}
+
+  ngOnInit(): void {}
+}
---
+++ mission-planning-dashboard/src/app/equipment/equipment.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-equipment',
+  templateUrl: './equipment.component.html',
+  styleUrls: ['./equipment.component.css', '../app.component.css'],
+})
+export class EquipmentComponent implements OnInit {
+  equipment: string[] = [
+    'Habitat dome',
+    'Drones',
+    'Food containers',
+    'Oxygen tanks',
+  ];
+  constructor() {}
+
+  ngOnInit(): void {}
+}
